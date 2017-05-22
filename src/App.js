40fa32771460fa12725339e3a40a29decd3bdf18
Jesstada <jesstada@gmail.com>
2017-05-22 16:24:37 +1000

    EVmap component added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     return (
       <div className="App">
           <h2>Welcome to GreenScreen</h2>
+          <iframe src="https://map.openchargemap.io/?mode=embedded" frameborder="0" height="500px" width="800px" >Map</iframe>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
